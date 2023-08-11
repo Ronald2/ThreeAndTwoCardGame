6b279f01e5cb1a367d2eec719c625b53d405cda4
Ronald Baez <ronaldbaezh@gmail.com>
2023-08-11 19:44:25 -0400

    changed the method Console.WriteLine() with the method made for this.

--- ThreeAndTwo/UI/ConsoleUI.cs
+++ ThreeAndTwo/UI/ConsoleUI.cs
@@ -6,17 +6,17 @@ namespace ThreeAndTwo.UI
         public static int GetNumberOfPlayers()
         {
             int numOfPlayers = 0;
-            Console.Write("How many players? ");
+            DisplayMessage("How many players? ");
             while (!int.TryParse(Console.ReadLine(), out numOfPlayers) || numOfPlayers <= 0 || numOfPlayers > 5)
             {
-                Console.Write("Please enter a valid number of players (max 5 players): ");
+                DisplayMessage("Please enter a valid number of players (max 5 players): ");
             }
             return numOfPlayers;
         }
 
         public static string GetPlayerName(int index)
         {
-            Console.Write($"Enter player - {index + 1} name: ");
+            DisplayMessage($"Enter player - {index + 1} name: ");
             string playerName = Console.ReadLine();
             return string.IsNullOrWhiteSpace(playerName) ? $"Player {index + 1}" : playerName;
         }
@@ -34,23 +34,25 @@ namespace ThreeAndTwo.UI
 
         public static void DisplayGameInstructions()
         {
-            Console.WriteLine("Welcome to Three and Two Card Game!\n");
-            Console.WriteLine("Objective: The goal of the game is to collect a hand with three cards of the same rank (three-of-a-kind) and two cards of another rank (a pair).");
-            Console.WriteLine("           The first player to achieve this combination wins the game.");
-            Console.WriteLine("\nHow to Play:");
-            Console.WriteLine("1. Enter the number of players participating in the game (should be greater than 0), max 5 players.");
-            Console.WriteLine("2. Enter the name of each player when prompted (Default name: Player).");
-            Console.WriteLine("3. The game will automatically deal cards to each player.");
-            Console.WriteLine("4. During each player's turn, they will draw a card from the deck and can choose to discard a card from their hand.");
-            Console.WriteLine("5. The game will continue until a player forms a winning hand or until the deck is empty.");
-            Console.WriteLine("6. The winning player will be announced, and the game will end.\n");
-            Console.WriteLine("Enjoy playing the Three and Two Card Game!");
-            Console.WriteLine("----------------------------------------\n");
+            DisplayMessage("Welcome to Three and Two Card Game!\n");
+            DisplayMessage("Objective: The goal of the game is to collect a hand with three cards of the same rank (three-of-a-kind) and two cards of another rank (a pair).");
+            DisplayMessage("           The first player to achieve this combination wins the game.");
+            DisplayMessage("\nHow to Play:");
+            DisplayMessage("1. Enter the number of players participating in the game (should be greater than 0), max 5 players.");
+            DisplayMessage("2. Enter the name of each player when prompted (Default name: Player).");
+            DisplayMessage("3. The game will automatically deal cards to each player.");
+            DisplayMessage("4. During each player's turn, they will draw a card from the deck and can choose to discard a card from their hand.");
+            DisplayMessage("5. The game will continue until a player forms a winning hand or until the deck is empty.");
+            DisplayMessage("6. The winning player will be announced, and the game will end.\n");
+            DisplayMessage("Enjoy playing the Three and Two Card Game!");
+            DisplayMessage("----------------------------------------\n");
         }
 
         public static void ClearScreen()
         {
             Console.Clear();
         }
+
+
     }
 }
